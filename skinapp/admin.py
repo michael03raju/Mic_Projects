523e2fc344c402176ee96fa54e7e3935633359f0
michael03raju <99629989+michael03raju@users.noreply.github.com>
2025-04-06 16:10:59 +0100

    Delete skinapp directory

--- skinapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Appointment,Doctor_details
-# Register your models here.
-
-admin.site.register(Appointment)
-admin.site.register(Doctor_details)
--- skinapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SkinappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'skinapp'
